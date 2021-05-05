dd42dd2a8a701f542be991ba97f5389fca825966
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-05 02:04:53 -0400

    add additional functions reflecting notes about parameters, arguments, and the ES6 addition of default parameters.

--- javascript_functions.js
+++ javascript_functions.js
@@ -6,9 +6,32 @@
  /* 1 */ /* 2 */
 function greet() {
     console.log('Hello!'); /* 3 */
-}
+};
+
+greet();
+
+
 
 //Function Declaration with Parameters
+                  /*    4     */
+function calcArea(width, height) {
+    let calculatedArea = width * height;
+    let message = `The total area calculated is ${calculatedArea} ft.`;
+    console.log(message);
+};
+        /* 5 */
+calcArea(5,9);
+
+
+
+//Function Declaration with Default Parameters
+                /*      6       */
+function greeting(name = 'my dear fellow') {
+    console.log(`Hello ${name}!`);
+};
+
+greeting();
+greeting('Hamza');
 
 
 
@@ -19,3 +42,6 @@ function greet() {
 // 1. KEYWORD
 // 2. IDENTIFIER: When calling the declared function, you simply call the identify with the round brackets. Example: `greet();`
 // 3. FUNCTION BODY: The code block with the code executed upon the function's call.
+// 4. PARAMETERS: Parameters are declared when the function is declared & indicate that the function can accept input(s) & use those inputs in some way.
+// 5. ARGUMENTS: Values or variables are passed into the round brackets of the called function which had parameters declared with it. These arguments follows the order of the parameters that were declared.
+// 6. DEFAULT PARAMETER(S): ES6 introduces the ability to set predetermined values for parameters that will be used if an argument is not passed to a called function.
