e0ed7ce6ff48bc2db0c954e643048b4c071b81e2
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-05 01:03:34 -0400

    add notes for a basic function declaration.

---
+++ javascript_functions.js
@@ -0,0 +1,21 @@
+/*-----------------------------------
+~~~| What is a FUNCTION DECLARATION? |~~~
+-------------------------------------- */
+
+//Basic Function Declaration
+ /* 1 */ /* 2 */
+function greet() {
+    console.log('Hello!'); /* 3 */
+}
+
+//Function Declaration with Parameters
+
+
+
+/*---------
+~~~| INDEX |~~~
+------------ */
+
+// 1. KEYWORD
+// 2. IDENTIFIER: When calling the declared function, you simply call the identify with the round brackets. Example: `greet();`
+// 3. FUNCTION BODY: The code block with the code executed upon the function's call.
