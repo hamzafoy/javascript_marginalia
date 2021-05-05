2f61a3772926765d95ef63799b2d939f9dbbd1d1
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-05 04:04:30 -0400

    add notes on JS scope.

--- js_functions.js
+++ js_functions.js
@@ -105,10 +105,10 @@ const conciseSquared = num => num * num;
 ------------ */
 
 // 1. KEYWORD
-// 2. IDENTIFIER: When calling the declared function, you simply call the identify with the round brackets. Example: `greet();`
+// 2. IDENTIFIER: When calling the defined function, you simply call the identifier with parentheses. Example: `greet();`
 // 3. FUNCTION BODY: The code block with the code executed upon the function's call.
-// 4. PARAMETERS: Parameters are declared when the function is declared & indicate that the function can accept input(s) & use those inputs in some way.
-// 5. ARGUMENTS: Values or variables are passed into the round brackets of the called function which had parameters declared with it. These arguments follows the order of the parameters that were declared.
+// 4. PARAMETERS: Parameters are defined when the function is defined & indicate that the function can accept input(s) & use those inputs in some way.
+// 5. ARGUMENTS: Values or variables are passed into the round brackets of the called function which had parameters defined with it. These arguments follows the order of the parameters that were defined.
 // 6. DEFAULT PARAMETER(S): ES6 introduces the ability to set predetermined values for parameters that will be used if an argument is not passed to a called function.
 // 7. RETURN STATEMENT: The return statement ensures a function produces an output. Upon the use of a return statement, the execution of the rest of the function is stopped and the code that follows is not executed.
 // 8. HELPER FUNCTIONS: The use of return value of a function inside another function allow for tasks to be broken up, easier to read & debug, and are known as helper functions.
---
+++ js_scope.js
@@ -0,0 +1,40 @@
+/*-----------------------------
+~~~| What is the GLOBAL SCOPE? |~~~
+-------------------------------- */
+
+// Variables defined outside of specified code blocks are in the global scopes & often called global variables.
+// Since global variables are not bound inside a specified code block, they can be accessed by any code in the program including code in specified code blocks.
+
+//EXAMPLE
+
+const favoriteDessert = 'knafeh';
+
+const returnFavoriteDessert = () => favoriteDessert;
+
+console.log(returnFavoriteDessert());
+
+
+
+/*-----------------------------
+~~~| What is the GLOBAL SCOPE? |~~~
+-------------------------------- */
+
+//Variables defined inside a code block is only accessible to code within the same code block {}. These variables has block scope and are known as local variables.
+
+//EXAMPLE
+
+const returnFavoriteMovie = () => {
+    let favoriteMovie = 'The Shawshank Redemption';
+    console.log(favoriteMovie);
+}
+
+console.log(favoriteMovie); //Expect a 'ReferenceError', you are trying to read a value that is NOT in the global scope. Code outside the specified code block {} where the value is found cannot use said value.
+returnFavoriteMovie();
+
+
+
+/*---------
+~~~| INDEX |~~~
+------------ */
+
+// * Scope is the context in which variables are declared. Variables can exist either outside or inside specified code blocks {} such as belonging to variables, functions, or if statements.
