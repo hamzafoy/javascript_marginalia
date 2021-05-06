998eb9bc744a5727724d0da0cbe57ac562bf639c
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-06 18:59:43 -0400

    add more notes related to JavaScript arrays including properties and methods.

--- js_arrays.js
+++ js_arrays.js
@@ -18,6 +18,18 @@ console.log(listOfItems[1]); //Expected Output is 'gravy'
 //Updating the value of an element inside the array using 'bracket notation'.
 listOfItems[1] = 'Spaghetti';
 console.log(listOfItems[1]);
+//Do note that you can still update elements in an array that is defined to a variable using `const`, you just can't reassign a new array or new value to the `const`.
+
+
+//Array Properties
+                        /* 3 */
+console.log(listOfItems.length);
+
+
+//Array Methods
+           /*          4          */
+listOfItems.push('Rome: Total War'); //Note that `.push()` "mutates" the initial array, changing the array from its initial state.
+console.log(listOfItems);
 
 
 
@@ -27,3 +39,5 @@ console.log(listOfItems[1]);
 
 //1. ARRAY LITERAL: An array literal creates an array by wrapping individual 'elements' in square brackets. This can be saved to a variable.
 //2. INDEX: The numbered position of an element inside the array. JavaScript arrays are 'zero-indexed' which means the first element has an index of 0.
+//3. LENGTH PROPERTY: A built-in property of arrays, `.length` uses dot notation to return the number of elements in a checked array.
+//4. PUSH METHOD: A method used with arrays, the `.push()` method is given 1 or more arguments which are elements you want to add to the end of an array.
