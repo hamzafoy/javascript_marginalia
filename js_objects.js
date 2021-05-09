3f85488f66a58200674323f36076b2b9deea3521
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-08 22:49:29 -0400

    add file for notes on JavaScript objects.

---
+++ js_objects.js
@@ -0,0 +1,22 @@
+/*----------------------
+~~~| What is an OBJECT? |~~~
+------------------------- */
+
+//An object can be assigned to a variable. Whereas square brackets [] are used for 'array literals', curly brackets {} are used for objects.
+//Objects can be filled with data and will be organized as key-value pairs. Keys & their corresponding values are separated by a colon mark.
+//Keys are strings, but objects allow the omission of the quotation marks if no special characters exist in the key.
+
+/* OBJECT */
+let army = {
+    /* KEY */  /* Value */
+    infantry = '10000', //`infantry`, `cavalry`, `bowmen`, & `onagers` are all properties of this object.
+    cavalry = '200',
+    bowmen = '2000',
+    onagers = '12'
+}
+
+
+
+/*---------
+~~~| INDEX |~~~
+------------ */
