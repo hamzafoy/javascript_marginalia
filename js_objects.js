a0d0fe723d1d51a9fcb0cfe95bdd2b2e1083aae9
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-09 00:47:34 -0400

    add more notes on object manipulation.

--- js_objects.js
+++ js_objects.js
@@ -9,14 +9,28 @@
 /* OBJECT */
 let army = {
     /* KEY */  /* Value */
-    infantry: '10000', //`infantry`, `cavalry`, `bowmen`, & `onagers` are all properties of this object.
-    cavalry: '200',
-    bowmen: '2000',
-    onagers: '12'
-}
+    infantry: 10000, //`infantry`, `cavalry`, `bowmen`, & `onagers` are all properties of this object.
+    cavalry: 200,
+    bowmen: 2000,
+    onagers: 12
+};
 
 //You can access an object's properties using 'dot notation'.
-console.log(`My army has ${army.infantry} footmen & ${army.bowmen} archers.`);
+console.log(`My army has ${army.infantry} footmen & ${army.bowmen} archers.`); //EXPECTED OUTPUT is 'my army has 10000 footmen & 2000 archers'.
+
+//You can access an object's properties using 'bracket notation. The key inside the bracket MUST be passed as a string.
+console.log(`I am moving ${army['onagers']} artillery pieces on the hill overlooking the city's walls.`); //EXPECTED OUTPUT is 'I am moving 12 artillery pieces on the hill overlooking the city's walls'.
+
+//With bracket notation, you can pass a variable inside the brackets to select keys of a given object.
+let propReader = (obj, prop) => obj[prop];
+console.log(propReader(army, 'cavalry')); //EXPECTED OUTPUT is 200.
+
+//Assigning values to existing properties or assigning new properties. You can use either bracket or dot notation.
+army.mercenaries = 500;
+army['onagers'] = 24;
+console.log(army); //EXPECTED OUTPUT is 500 mercenaries added & 24 onagers.
+delete army['mercenaries'];
+console.log(army); //EXPECTED OUTPUT is army object absent any mercenaries.
 
 
 
