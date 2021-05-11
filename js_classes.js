77a335c7a537e55b66f7d52e69047c6c28c1e5e7
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-11 13:12:41 -0400

    fix notes on general class syntax.

--- js_classes.js
+++ js_classes.js
@@ -6,17 +6,22 @@
 //Classes allow you to specify the shared properties and methods of objects produced with said class & then you can 'extend' the class for objects that have the shared
 //properties/methods in addition to unique properties/methods. Objects created based on a class is known as an 'instance of the class'.
 
-class Army {
+class Levy {
+    /*                      1                      */
     constructor(infantry, cavalry, bowmen, onagers) {
         this.infantry = infantry;
         this.cavalry = cavalry;
         this.bowmen = bowmen;
         this.onagers = onagers;
     }
+    get roster() {
+        console.log(`This levy is a force of ${this.infantry} footmen, ${this.cavalry} riders, ${this.bowmen} archers, & ${this.onagers} pieces of artillery.`)
+    }
 };
 
-const legioapollonaris = new Army(17500, 500, 1250, 0);
-console.log(legioapollonaris);
+const garrisonTarentum = new Levy(17500, 500, 1250, 0);
+console.log(garrisonTarentum); //EXPECTED OUTPUT is Levy { infantry: 17500, cavalry: 500, bowmen: 1250, onagers: 0 }
+garrisonTarentum.roster; //EXPECTED OUTPUT is 'This levy is a force of 17500 footmen, 500 riders, 1250 archers, & 0 pieces of artillery.'
 
 
 
