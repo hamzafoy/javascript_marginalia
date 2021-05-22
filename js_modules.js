d23956914dd0c311989754dbc3be3ab9f99a52e6
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-22 05:01:10 -0400

    add a new note regarding JS modules.

---
+++ js_modules.js
@@ -0,0 +1,29 @@
+/*----------------------------------
+~~~| What is a RUNTIME ENVIRONMENT? |~~~
+------------------------------------- */
+
+//Runtime environments are where your program(s) are to be executed.
+//Which runtime environment your program runs on determines what GLOBAL OBJECTS your program can access.
+//In general, JavaScript is typically seen running on two runtime environments:
+// 1. BROWSER: includes Chrome, Firefox, & the eventually out-of-service Internet Explorer.
+// 2. NODE: 
+
+
+
+/*--------------------------------------------
+~~~| What is the BROWSER RUNTIME ENVIRONMENT? |~~~
+----------------------------------------------- */
+
+//Applications created for & executed in the browser are typically referred to as front-end applications.
+//Browsers usually have global objects such as `window` and can be accessed through your written JS programming.
+
+
+
+/*-----------------------------------------
+~~~| What is the NODE RUNTIME ENVIRONMENT? |~~~
+-------------------------------------------- */
+
+//Node, created in 2009, allowed programmers to develop apps that no longer relied on a browser.
+//Node gives access to global objects such as `process` which contains data related to a JavaScript file being executed.
+//Applications designed for the Node are usually back-end applications OR full-stack (front & back end) applications.
+//Back-end applications have access to file systems, databases, & networks attached to a server.
