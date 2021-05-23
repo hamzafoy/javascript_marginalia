33afeadad450a1b8fe071faeaedfb4a3a06660a7
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-22 22:28:51 -0400

    add additional notes regarding exact, optional matches and global and case insensitive modifiers.

--- js_reg_exp.js
+++ js_reg_exp.js
@@ -3,7 +3,7 @@
 ------------------------------------ */
 
 //Regular Expressions serve as patterns for searching text.
-//RegExp are useful for validating input, extracting specific part(s) of a string, & performing searches and replacing strings (or parts of a string).
+//RegEx are useful for validating input, extracting specific part(s) of a string, & performing searches and replacing strings (or parts of a string).
 
 //There are two ways to create a regular expression:
 // 1. LITERALS: `let expression = /any regular expression goes here/;
@@ -21,6 +21,26 @@ let exampleString = `The color of frankincense ranges from gray to gold and the 
 Frankincense has always been a luxury commodity even in the times of Antiquity, usually originating from lands such as Arabia Felix.
 Arabia Felix is modern day southern Yemen, noted for having arable land unlike most of the Arabian Peninsula.`
 
-let exactMatchTest = /Antiquity/;
+let exactMatchTest = /Antiquity/; //Exact matches are made by placing the search word between right-leaning slashes (/). This regex is ONLY searching for that specific word.
+let howManyExactMatches = exampleString.match(exactMatchTest);
+let exactMatchCounter = howManyExactMatches.length;
+let optionalMatchTest = /colou?r/g; //Optional matches are made by placing a ? to the right of letters that are optionally included in the search. This regex in the example will match UK & US spellings of color/colour.
+let howManyOptionalMatches = exampleString.match(optionalMatchTest)
+let optionalMatchCounter = howManyOptionalMatches.length;
+let globalModifierTest = /Arabia/g; //Global modifiers ensure that you are finding ALL matches instead of the default where it only finds the FIRST match.
+let caseInsensitiveModifierTest = /the/gi; //Case Insensitive modifiers matches what is being tested for including uppercase and lowercase
 
-exactMatchTest.test(exampleString) ? console.log(`This RegExp has matched`) : console.log(`This RegExp has no matches.`); //EXPECTED OUTPUT: `This RegExp has matched.`
+// console.log(exampleString.match(exactMatchTest)); //EXPECTED OUTPUT includes printing the word that was matched and its index.
+/*               1              */
+exactMatchTest.test(exampleString) ? console.log(`This RegExp has found ${exactMatchCounter} match(es)`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
+optionalMatchTest.test(exampleString) ? console.log(`This RegExp has found ${optionalMatchCounter} match(es)`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
+globalModifierTest.test(exampleString) ? console.log(`This RegExp has found a match`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
+caseInsensitiveModifierTest.test(exampleString) ? console.log(`This RegExp has found a match`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
+
+
+
+/*---------
+~~~| INDEX |~~~
+------------ */
+
+//1. TEST METHOD: `test()` is a method called on a variable storing a regular expression and the argument passed is the variable storing the string to test against the regular expression.
