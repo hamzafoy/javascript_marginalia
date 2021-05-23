5b36662dbeb05a0f707ae7465e7ffdd657a4992a
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-22 22:57:27 -0400

    make corrections to current notes on RegEx.

--- js_reg_exp.js
+++ js_reg_exp.js
@@ -18,7 +18,7 @@
 
 //EXAMPLE STRING
 let exampleString = `The color of frankincense ranges from gray to gold and the colour of the scent given off by its smoke is usually a light grey. 
-Frankincense has always been a luxury commodity even in the times of Antiquity, usually originating from lands such as Arabia Felix.
+Such frankincense has always been a luxury commodity even in the times of Antiquity, usually originating from lands such as Arabia Felix.
 Arabia Felix is modern day southern Yemen, noted for having arable land unlike most of the Arabian Peninsula.`
 
 let exactMatchTest = /Antiquity/; //Exact matches are made by placing the search word between right-leaning slashes (/). This regex is ONLY searching for that specific word.
@@ -27,15 +27,19 @@ let exactMatchCounter = howManyExactMatches.length;
 let optionalMatchTest = /colou?r/g; //Optional matches are made by placing a ? to the right of letters that are optionally included in the search. This regex in the example will match UK & US spellings of color/colour.
 let howManyOptionalMatches = exampleString.match(optionalMatchTest)
 let optionalMatchCounter = howManyOptionalMatches.length;
-let globalModifierTest = /Arabia/g; //Global modifiers ensure that you are finding ALL matches instead of the default where it only finds the FIRST match.
-let caseInsensitiveModifierTest = /the/gi; //Case Insensitive modifiers matches what is being tested for including uppercase and lowercase
+let globalModifierTest = /Arabia[^a-z]/g; //Global modifiers ensure that you are finding ALL matches instead of the default where it only finds the FIRST match.
+let howManyGlobalMatches = exampleString.match(globalModifierTest);
+let globalModifierCounter = howManyGlobalMatches.length;
+let caseInsensitiveModifierTest = /such/gi; //Case Insensitive modifiers matches what is being tested for including uppercase and lowercase.
+let howManyCaseInsensitiveMatches = exampleString.match(caseInsensitiveModifierTest);
+let caseInsensitiveCounter = howManyCaseInsensitiveMatches.length;
 
 // console.log(exampleString.match(exactMatchTest)); //EXPECTED OUTPUT includes printing the word that was matched and its index.
 /*               1              */
 exactMatchTest.test(exampleString) ? console.log(`This RegExp has found ${exactMatchCounter} match(es)`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
 optionalMatchTest.test(exampleString) ? console.log(`This RegExp has found ${optionalMatchCounter} match(es)`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
-globalModifierTest.test(exampleString) ? console.log(`This RegExp has found a match`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
-caseInsensitiveModifierTest.test(exampleString) ? console.log(`This RegExp has found a match`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
+globalModifierTest.test(exampleString) ? console.log(`This RegExp has found ${globalModifierCounter} match(es)`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
+caseInsensitiveModifierTest.test(exampleString) ? console.log(`This RegExp has found ${caseInsensitiveCounter} match(es)`) : console.log(`This RegExp has found no matches.`); //EXPECTED OUTPUT: `This RegExp found a match.`
 
 
 
