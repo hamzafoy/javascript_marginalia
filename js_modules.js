24f9d555d6e99cbde351282229c20ec25627e956
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-26 12:36:30 -0400

    add more notes regarding modules and the ES6 v Node.js syntax.

--- js_modules.js
+++ js_modules.js
@@ -27,3 +27,20 @@
 //Node gives access to global objects such as `process` which contains data related to a JavaScript file being executed.
 //Applications designed for the Node are usually back-end applications OR full-stack (front & back end) applications.
 //Back-end applications have access to file systems, databases, & networks attached to a server.
+
+
+
+/*---------------------
+~~~| What is a MODULE? |~~~
+------------------------ */
+
+//A module is a reuseable piece, block, or file of code that can be exported then imported for use in a program or programs.
+//The ability to separate components of code, use them individually or combine them in any number of ways makes for a MODULAR PROGRAM.
+//Separating code to create modular programs is sometimes referred to as 'separation of concerns' ahd have the following benefits:
+// 1. Makes for easier debugging and fixing of code.
+// 2. Allows the recycling of defined logic in different parts of your program.
+// 3. Prevents pollution of the global scope & namespace and avoids naming collisions for created variables.
+
+//MODULES can be implemented in two ways:
+// 1. ES6 Syntax: `import` & `export`.
+// 2. Vanilla Node.js syntax: `modules.export` & `require()` method.
