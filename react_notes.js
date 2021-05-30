eef2df1f3d1297bbc12ab90662c13e444f23f467
Ross Foy <hamzaoflouisville@gmail.com>
2021-05-30 12:06:28 -0400

    add notes taken about the React JS library.

---
+++ react_notes.js
@@ -0,0 +1,5 @@
+/*------------------
+~~~| What is REACT? |~~~
+--------------------- */
+
+//React is a JavaScript library that helps with building user interfaces. This library breaks the web app up into components making for easier readability and editing.
