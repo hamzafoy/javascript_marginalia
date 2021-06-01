5f6baf4a10c1f05a9c51ba01d22927b1d284a626
Ross Foy <hamzaoflouisville@gmail.com>
2021-06-01 03:58:27 -0400

    add notes regarding GET/POST requests with AJAX.

--- js_http.js
+++ js_http.js
@@ -26,3 +26,41 @@
 //2. The domain name `europabarbarorum.com` is recognized by the internet's Domain Name Server and an IP [Internet Protocol] address is returned.
 //3. The client uses the IP address and opens a TCP connection to the server at that address and initiate its REQUEST.
 //4. Please note that every resource request requires a separate connection to the server using HTTP/1.0 whereas HTTP/1.1 can use one connection more than once causing less delay in communication.
+
+//JavaScript uses an EVENT LOOP to handle async function calls. Function calls are made and added to a stack, async functions are sent to a separate queue. Once the stack has cleared, the functions in the queue are executed.
+
+
+
+/*-----------------
+~~~| What is AJAX? |~~~
+-------------------- */
+
+//Asynchronous JavaScript & XML [AJAX] enables requests to be made after the initial page load. AJAX can be used to handle XML, JSON, & more.
+
+//The following is boilerplate AJAX for a GET request:
+
+const xhr = new XMLHttpRequest();
+const url = `http://api-to-call.com/endpoint`;
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+    if(xhr.readyState = XMLHttpRequest.DONE) {
+        return xhr.response;
+    }
+}
+xhr.open('GET', url);
+xhr.send();
+
+
+//The following is boilerplate AJAX for a POST request:
+
+const xhr = new XMLHttpRequest();
+const url = `http://api-to-call.com/endpoint`;
+const data = JSON.stringify({id: '200'});
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+    if(xhr.readyState = XMLHttpRequest.DONE) {
+        return xhr.response;
+    }
+}
+xhr.open('POST', url);
+xhr.send(data);
