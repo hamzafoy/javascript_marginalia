3262cc0a9ca23968f3ad9cbab039cccd62249ed7
Ross Foy <hamzaoflouisville@gmail.com>
2021-06-01 20:50:30 -0400

    add notes about JSX syntax.

--- react_notes.js
+++ react_notes.js
@@ -3,3 +3,32 @@
 --------------------- */
 
 //React is a JavaScript library that helps with building user interfaces. This library breaks the web app up into components making for easier readability and editing.
+
+
+
+/*----------------------
+~~~| How does JSX work? |~~~
+------------------------- */
+
+//A basic unit of JSX is called a JSX element. JSX elements look exactly like HTML, the only difference is that you would find it in a JavaScript file.
+//JSX is a syntax extension for JavaScript, written to be used with React. When a JS file contains JSX code, that file will have to be compiled.
+//A JSX compilar will translate any JSX into regular JS before reaching the web browser.
+//JSX elements are treated as JS expressions - hence JSX can be stored in a variable, array, or object & passed to a function.
+
+//EXAMPLES:
+
+//   <p>This is a paragraph</p>
+
+//   const nav = <nav>This is a navigation bar</nav>
+
+/*---
+
+    const myTeam = {
+        center: <li>Joel Embiid</li>,
+        powerForward: <li>LeBron James</li>,
+        smallForward: <li>Kevin Durant</li>,
+        shootingGuard: <li>Klay Thompson</li>,
+        pointGuard: <li>Chris Paul</li>
+    }
+
+---*/
