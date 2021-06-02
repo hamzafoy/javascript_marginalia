f0c17297783ec689140acbb1d5510757418837de
Ross Foy <hamzaoflouisville@gmail.com>
2021-06-01 21:38:11 -0400

    add more examples of JSX.

--- react_notes.js
+++ react_notes.js
@@ -14,12 +14,51 @@
 //JSX is a syntax extension for JavaScript, written to be used with React. When a JS file contains JSX code, that file will have to be compiled.
 //A JSX compilar will translate any JSX into regular JS before reaching the web browser.
 //JSX elements are treated as JS expressions - hence JSX can be stored in a variable, array, or object & passed to a function.
+//JSX elements can have attributes, can be nested, but. . .
+//A JSX expression MUST have exactly ONE outermost element.
 
 //EXAMPLES:
 
 //   <p>This is a paragraph</p>
 
 //   const nav = <nav>This is a navigation bar</nav>
+
+//   const imgGrande = <img src="img/big.svg" alt="Giant" width="500px" height="500px"/>
+
+/*---
+
+const anchorGoogle = (
+    <a href="https://www.google.com">
+        <h1>
+            Click me!
+        </h1>
+    </a>
+);
+
+NOTE: JSX elements that take up more than one line should be wrapped in parentheses.
+
+---*/
+
+/*---
+
+const anchorGoogle = (
+    <a href="https://www.google.com">
+        <h1>
+            Click me!
+        </h1>
+    </a>
+);
+^ THIS WORKS
+
+const failure = (
+    <p>This is a</p>
+    <p?FAILURE!</p>
+)
+^ THIS DOES NOT WORK
+
+The first opening tag & final closing tag of a JSX expression MUST belong to the SAME JSX element. An easy fix is to enclose the above failed example in a <div></div> tag.
+
+---*/
 
 /*---
 
