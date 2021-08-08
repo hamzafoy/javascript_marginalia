3571cd59be4183648b283cd17523040b3b13c8bc
Ross Foy <hamzaoflouisville@gmail.com>
2021-08-08 17:20:32 -0400

    add notes from Mintbean team layout hackathon in early July.

---
+++ html_css_notes.js
@@ -0,0 +1,25 @@
+/*-------------------------------------
+~~~| Miscellaneous Notes on HTML5/CSS3 |~~~
+---------------------------------------- */
+
+/*--
+1. background-image {
+
+}
+will not allow screen-readers to see `alt` properties. use <img/> tags instead for accessibility.
+--*/
+
+/*--
+2. HTML naming convention should make plain & apparent what are parent elements & their children.
+
+EX:
+<div class="card">Parent Element
+    <div class="card__title">
+    Child Element
+    </div>
+</div>
+--*/
+
+/*--
+3. Use `min-height` to set height constraints instead of `height` to avoid overflow.
+--*/
